e6fcca73d266e596f3722f97a63baca9e9e769b7
Martial Bret <martial.bret@fms-ea.com>
2024-11-15 12:07:02 +0100

    Ajout de documentation des URLs

--- competence_exchange/core/urls.py
+++ competence_exchange/core/urls.py
@@ -3,7 +3,9 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('creaneaux-disponibles/', views.available_slots, name='available_slots'),
+    # page des créneaux d'aide proposés par tousles utilisateurs
+    path('creneaux-disponibles/', views.available_slots, name='available_slots'),
+    # ...
     path('competences/', views.competence_list, name='competence_list'),
     path('connexion/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('deconnexion/', auth_views.LogoutView.as_view(), name='logout'),
